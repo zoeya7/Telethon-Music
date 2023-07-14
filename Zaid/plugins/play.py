4dd7ef05d484299a2acba72efee025c927f05843
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-14 15:56:56 +0530

    Update button for channel in play.py

--- Zaid/plugins/play.py
+++ Zaid/plugins/play.py
@@ -128,7 +128,7 @@ async def _(event):
      await event.delete()
 
 btnn =[
-    [Button.url("sᴜᴘᴘᴏʀᴛ ⚙️", url=f"t.me/{Config.SUPPORT}"), Button.url("cʜᴀɴɴᴇʟ 🇮🇳", url=f"t.me/{Config.CHANNEL}")],
+    [Button.url("sᴜᴘᴘᴏʀᴛ ⚙️", url=f"t.me/Musicbot_update"), Button.url("cʜᴀɴɴᴇʟ 📂", url=f"t.me/aliaxtc")],
     [Button.inline("cʟᴏꜱᴇ 🗑️", data="cls")]]
 
 
