93b962f711dce8a5527bbae5f05a716c2e9fea0e
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-18 14:04:42 +0530

    Update ping.py

--- Zaid/plugins/ping.py
+++ Zaid/plugins/ping.py
@@ -36,5 +36,5 @@ async def _(event):
    delta_ping = time() - start
    uptime_sec = (current_time - START_TIME).total_seconds()
    uptime = await _human_time_duration(int(uptime_sec))
-   UMM = [[Button.url("⚜ Cԋαɳɳҽʅ ⚜", "https://t.me/TheUpdatesChannel")]]
+   UMM = [[Button.url("⚜ Cԋαɳɳҽʅ ⚜", "https://t.me/Musicbot_update")]]
    await event.reply(f"╰☞ 𝗣𝗢𝗡𝗚™╮\n☞ {delta_ping * 1000:.3f}\n☞ {uptime}", buttons=UMM)
