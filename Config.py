a5edd445dc196bcf48f50b54706a10b5b6e9a48b
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-19 21:18:04 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,7 +11,7 @@ class Config(object):
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/612b4483b2ed270c4070a.jpg")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://i.ibb.co/PGFHWDr/MUSIC-BOT.png")
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "6343636554")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
     AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
