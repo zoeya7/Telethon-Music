6ba603e757d8edfb402ce74095838c888647e307
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-20 16:32:25 +0530

    Update play.py

--- Zaid/plugins/play.py
+++ Zaid/plugins/play.py
@@ -128,7 +128,7 @@ async def _(event):
      await event.delete()
 
 btnn =[
-    [Button.url("sᴜᴘᴘᴏʀᴛ ⚙️", url=f"t.me/Musicbot_update"), Button.url("cʜᴀɴɴᴇʟ 📂", url=f"t.me/aliaxtc")],
+    [Button.url("sᴜᴘᴘᴏʀᴛ ⚙️", url=f"t.me/Musicbot_update"), Button.url("cʜᴀɴɴᴇʟ 📂", url=f"https://t.me/+EG3mh-vS9Y82N2Rl")],
     [Button.inline("cʟᴏꜱᴇ 🗑️", data="cls")]]
 
 
